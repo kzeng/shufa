0f64e4b176331d54b6d098ee93939e2fb01fa48f
zengkai <zengkai001@qq.com>
2026-08-24 10:17:08 +0800

    feat: add MainActivity with Compose setup

---
+++ app/src/main/java/com/example/shufa/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.shufa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.example.shufa.navigation.ShufaNavGraph
+import com.example.shufa.ui.theme.ShufaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShufaTheme {
+                val navController = rememberNavController()
+                ShufaNavGraph(navController = navController)
+            }
+        }
+    }
+}
